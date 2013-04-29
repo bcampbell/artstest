6942590cb2b8bf427d908e1d089f55ecb53e8a9b
Ben Campbell <ben@scumways.com>
2013-04-29 14:46:45 +1200

    add author checking to comparison tool

--- batch.sh
+++ batch.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# script to run scraper on a set of test articles and check results
+# against expected data
 
 set -e
+# the command to nvoke to scrape
 scrape=/home/ben/mygo/src/arts/scrapetool/scrapetool
+
+# command to compare scrape output against .expected files
 compare=./compare
 
 if [ -z "$1" ]
 then
+    # default - scan data dir
     inputs=$(find data -iname "*.expected")
+elif [ -d "$1" ]; then
+    # param is an alternative directory to scan
+    inputs=$(find "$1" -iname "*.expected")
 else
+    # param is an individual .expected file to run
     inputs="$1"
 fi
 
